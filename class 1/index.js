a1a8e0aa480892d1a0bec1050bc936bf4fe10f8a
Suvam093 <139885056+Suvam093@users.noreply.github.com>
2023-08-15 23:22:49 +0530

    starting java-script
    learning some basic concepts of java-script

---
+++ class 1/index.js
@@ -0,0 +1,8 @@
+//here we will write a code to print some stuffs in the console 
+console.log('Hello dosto 2');
+
+let a=5;    //let is a normal declaration
+console.log(a);
+
+var b=10; 
+console.log(b);
