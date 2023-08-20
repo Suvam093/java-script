8919ad481c50e76682c123a37aa6aa3f77da87fe
Suvam093 <139885056+Suvam093@users.noreply.github.com>
2023-08-20 22:40:37 +0530

    Update index.js
    learning about construction function and construction property with how the inbuilt function works to create a function as objects and learning the difference between the primitive and reference types of data, they can be updated in and used inside a function

--- class 2/index.js
+++ class 2/index.js
@@ -16,16 +16,92 @@ console.log(5);
 //     }
 // }
 
-function createRectangle(len,bre){
-    return rectangle={
-        length:len,
-        breadth:bre,
 
-        draw() {
-            console.log('drawing multiple rectangle using function');
-        }
+// factory function
+
+
+// function createRectangle(len,bre){
+//     return rectangle={
+//         length:len,
+//         breadth:bre,
+
+//         draw() {
+//             console.log('drawing multiple rectangle using function');
+//         }
+//     };
+// }
+
+// let rectangle1=createRectangle(5,4);
+// let rectangle2=createRectangle(2,3);
+
+
+// construction function and dynamic nature of an object
+
+
+// function Rectangle(l,b){
+//     this.length = l;
+//     this.breadth = b;
+//     this.draw = function(){
+//         console.log('drawing multiple rectangle using functions');
+
+//     };
+// }
+// let rectangleObj = new Rectangle(2,5);
+// rectangleObj.color='yellow';
+// console.log(rectangleObj);
+
+// delete rectangleObj.color;
+// console.log(rectangleObj);
+
+
+//we can use any of the two methods two create a function
+
+// constrictor property
+
+function CreateRectangle(length,breadth){
+    this.length =length;
+    this.breadth=breadth;
+    this.draw = function(){
+        console.log('construction property');
     };
-}
+};
 
-let rectangle1=createRectangle(5,4);
-let rectangle2=createRectangle(2,3);
+let rect =new Function('length', 'breadth',`this.length =length;
+this.breadth=breadth;
+this.draw = function(){
+    console.log('construction property');
+};`)
+let rectangle1 = new rect(2,7);
+console.log(rectangle1)
+
+
+//primitive and reference types in js
+
+let a = 10;
+let b=a;
+a++;
+console.log(a);
+console.log(b);
+
+
+//reference type in js
+let c = {value : 10};
+let d = c;
+c.value++;
+console.log(c.value);
+console.log(d.value);
+
+
+let f = 20;
+function inc(f){
+    f++;
+}
+inc(f);
+console.log(f);
+
+let g = {value:30};
+function incre(g){
+    g.value++;
+}
+incre(g);
+console.log(g.value);
